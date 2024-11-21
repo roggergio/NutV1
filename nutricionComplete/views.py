51b89300390b08d6e55422fd80d0a5be7674adf3
adairpena <31298918+adairpena@users.noreply.github.com>
2024-11-21 14:47:21 -0600

    change in template carrito and price

--- nutricionComplete/views.py
+++ nutricionComplete/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 def login(request):
     return render(request, 'login.html')
+def price(request):
+    return render(request, 'price.html')
 def registroNutri(request):
     return render(request, 'registroNutri.html')
+
+def carrito(request):
+    return render(request, 'carrito.html')
 def aside_1(request):
     return render(request, 'aside_1.html')
 def aside_2(request):
     return render(request, 'aside_2.html')
 def navBar(request):
     return render(request, 'navBar.html')
-def price(request):
-    return render(request, 'price.html')
+
 
 def pacientesList(request):
     return render(request, 'pacientesList.html')
