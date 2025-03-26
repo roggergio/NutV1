218883ef6749c29e1ed818065b2fc0c8df95f3f9
grpCondusef <grpcondusef@gmail.com>
2025-03-26 10:05:05 -0600

    update template navBar and urls in app pacientes

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_pacientes, name='lista_pacientes'),
+    path('lista_pacientes', views.lista_pacientes, name='lista_pacientes'),
     path('crear/', views.crear_paciente, name='crear_paciente'),
     path('editar/<int:paciente_id>/', views.editar_paciente, name='editar_paciente'),
     path('eliminar/<int:paciente_id>/', views.eliminar_paciente, name='eliminar_paciente'),
