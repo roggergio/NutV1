62e764de8408159a7cd03a36d4d51f2f6d6ab5ce
grpCondusef <grpcondusef@gmail.com>
2025-04-03 09:38:50 -0600

    add loginrequired in bioquimicos an enfermedades

--- bioquimicosConsultar/views.py
+++ bioquimicosConsultar/views.py
@@ -1,13 +1,15 @@
 from django.views.generic import ListView, DetailView
 from .models import Bioquimico
-
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from .models import Bioquimico
 
+@login_required
 def listar_bioquimicos(request):
     bioquimicos = Bioquimico.objects.all()
     return render(request, 'bioquimicosConsultar/listarBioquimicos.html', {'bioquimicos': bioquimicos})
 
+@login_required
 def detalle_bioquimico(request, bioquimico_id):
     bioquimico = get_object_or_404(Bioquimico, pk=bioquimico_id)
     return render(request, 'bioquimicosConsultar/detalleBioquimico.html', {'bioquimico': bioquimico})
